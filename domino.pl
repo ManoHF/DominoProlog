623f9f56a8c1d4d2fede079374d80a39454b6ca3
KarenArteaga <69361149+KarenArteaga@users.noreply.github.com>
2022-04-05 18:44:42 -0500

    Update domino.pl

--- domino.pl
+++ domino.pl
@@ -1,13 +1,12 @@
-ejemplo:- % ejemplo de como usar variables globales con nb_setvar(nombre, Valor).
-	nb_setval(lista, [1,2,3,4,5,6,7,8,9,0]), % se asigna [1,2,3,4,5,6,7,8,9,0] a la lista 1
-	nb_getval(lista, K), % se encuentra una K que sea el valor de lista
-	write(K), % K es [1,2,3,4,5,6,7,8,9,0]
-	nb_setval(lista, [28,2,3,4,5,6,7,8,9,0]), % el nuevo valor de tablero es [28,2,3,4,5,6,7,8,9,0]
-	nb_getval(lista, P), % se encuentra una P que sea el valor de lista
-	write(P). % P es [28,2,3,4,5,6,7,8,9,0]
+ejemplo:-
+	nb_setval(lista, [1,2,3,4,5,6,7,8,9,0]),
+	nb_getval(lista, K),
+	write(K),
+	nb_setval(lista, [28,2,3,4,5,6,7,8,9,0]). % el nuevo valor de tablero es [28,2,3,4,5,6,7,8,9,0]
 
-% aquí empieza el juego
+
 main:- 
+	nb_setval(fichasOp, [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[2,2],[2,3],[2,4],[2,5],[2,6],[3,3],[3,4],[3,5],[3,6],[4,4],[4,5],[4,6],[5,5],[5,6],[6,6]]),
 	nb_setval(numFichas, 7),
 	nb_setval(numFichasOp, 7),
 	nb_setval(tablero, []), % el tablero empieza vací
@@ -15,11 +14,15 @@ main:-
 	write("Ingresa tus fichas: "), nl,
 	read(A), nl,
 	nb_setval(fichas, A),
+	nb_getval(fichasOp, Op),
+	write("fichas que no han salido: "), write(Op), nl,
+	eliminarLista(A, Op, NOp),
+	nb_setval(fichasOp, NOp),
+	write("fichas que no han salido: "), write(NOp), nl,
 	mula(A, 6, T), nl,
 	jugar(T).
 
-% encontrar la mula más alta y asigna el turno al otro
-mula(Fichas, N, T):- 
+mula(Fichas, N, T):-
 	(	
 		(
 			buscar([N, N], Fichas), 
@@ -49,12 +52,10 @@ jugar(1):-
 	nb_getval(tablero, Tab),
 	write("el tablero se ve así: "), write(Tab), nl,
 	nb_getval(fichas, Fichas),
-	write(Fichas),
 	nb_getval(tablero, [A|B]),
-	write([A|B]),
 	last(B, C),
-	write("tablero: "), write([A, C]), nl,
-	write("fichas: "), write(Fichas), nl,
+	write("primera y ultima ficha del tablero: "), write([A, C]), nl,
+	write("tus fichas: "), write(Fichas), nl,
 	tirar(), nl,
 	jugar(0).
 
@@ -63,28 +64,21 @@ jugar(0):-
 	write("el tablero se ve así: "), write(Tab), nl,
 	write("¿Cuántas comió el oponente?"), nl,
 	read(N),
-	write("Escribe -1 si el oponente tiró en la cabeza, 0 si no tiró o 1 si tiró en la cola"), nl,
+	write("Escribe -1 si el oponente tiró en la cabeza, 0 si pasó, o 1 si tiró en la cola"), nl,
 	read(U),
 	oponenteTiro(N, U), nl,
 	jugar(1).
 
-oponenteTiro(N, 1):-
-	nb_getval(tablero, Tab),
+oponenteTiro(N, V):-
 	write("¿Qué tiró el oponente? "), nl,
-	read(C),
-	combina(Tab,[C], NT),
-	nb_setval(tablero, NT),
+	read(F),
+	acomodar(V, F),
+	nb_getval(fichasOp, Op),
+	eliminar(F, Op, NOp),
+	nb_setval(fichasOp, NOp),
+	write("fichas que no han salido: "), write(NOp), nl,
 	nb_getval(numFichasOp, M),
-	NuevoNum = N + M - 1,
-	nb_setval(numFichasOp, NuevoNum).
-
-oponenteTiro(N, -1):-
-	nb_getval(tablero, Tab),
-	write("¿Qué tiró el oponente? "), nl,
-	read(C),
-	nb_setval(tablero, [C|Tab]),
-	nb_getval(numFichasOp, M),
-	NuevoNum = N + M - 1,
+	NuevoNum = (N + M - 1),
 	nb_setval(numFichasOp, NuevoNum).
 
 oponenteTiro(N, 0):-
@@ -92,37 +86,78 @@ oponenteTiro(N, 0):-
 	NuevoNum = N + M,
 	nb_setval(numFichasOp, NuevoNum).
 
+acomodar(-1, F):- % acomodar en la cabeza
+	nb_getval(tablero, Tab),
+	nb_setval(tablero, [F|Tab]).
+
+acomodar(1, F):- % acomodar en la cola
+	nb_getval(tablero, Tab),
+	union(Tab,[F], NT),
+	nb_setval(tablero, NT).
+
+acomodar(2, F):-
+	nb_getval(tablero, [[A1|_]|B]),
+	cabeza(F, HF), last(F, TF),
+	last(B, TT), % cola del tablero.
+	HF =:= A1, write([TF,HF]),nl, acomodar(-1,[TF,A1]);
+	!.
+
+
+
+
 tirar():-
 	nb_getval(fichas, Fichas),
 	nb_getval(tablero, [[A1|_]|B]),
-	encontrar(Fichas, A1, [], D),
+	encontrar(Fichas, A1, [], D), % D es la lista de las fichas que se pueden acomodar en la cabeza
 	last(B, L),
 	last(L, L1),
-	encontrar(Fichas, L1, D, PF),
-	((PF == [], comer(Fichas, 7));
-	(PF \= [],
-	write("Posibles tiros: "), write(PF), nl)).
+	encontrar(Fichas, L1, D, PF), % PF es posibles fichas
+	((PF == [], 
+		comer(Fichas, 7));
+	(length(PF, 1), write("solo puedes tirar: "),
+	 	cabeza(PF, F), eliminar(F, Fichas, NuevasFichas), nb_setval(fichas, NuevasFichas), acomodar(2, F));
+	(write("Posibles tiros: "), write(PF), nl)).
 
 	% minimax con posibles tiros (encuentra la mejor pieza)
 	% actualizar tablero
 
-
+encontrar([], _, PF, PF):- !.
 encontrar([[A|B]|F], Val, C, PF):-
 	((A =:= Val; B =:= Val),
 	write("encontré: "), write([A|B]), nl,
-	combina(C, [[A|B]], N),
+	union(C, [[A|B]], N),
 	encontrar(F, Val, N, PF));
 	encontrar(F, Val, C, PF).
 
-encontrar([], _, PF, PF):- !.
 
 comer(A, L):-
 	write("¿Qué ficha comiste?"), nl,
-	read(B),
-	nb_setval(fichas, [B|A]),
-	LN is L + 1,
+	read(F),
+	nb_setval(fichas, [F|A]),
+	nb_getval(fichasOp, Op),
+	eliminar(F, Op, NOp),
+	nb_setval(fichasOp, NOp),
+	write("fichas que no han salido: "), write(NOp), nl,
+	LN is (L + 1),
 	nb_setval(numFichas, LN),
 	tirar().
+
+eliminar(F, L1, L):-
+	(cabeza(F, A),
+	last(F, B),
+	B < A,
+	delete(L1, [B,A], L),!);
+	delete(L1, F ,L).
+
+eliminarLista([H|T], L, NL):-
+	eliminar(H, L, L3),
+	eliminarLista(T, L3, NL).
+eliminarLista([], L, L):- !.
+
+
+
+cabeza([A|_], A).
+
 
 len([], 0).
 len([_|A], L):-
@@ -130,12 +165,7 @@ len([_|A], L):-
   	L is L2 + 1.
 
 
-combina([], L, L):-
+miembro(X, [X|_]):-
 	!.
-combina([X|L1], L2, [X|L3]):-
-	combina(L1, L2, L3).
-
-
-
-
-
+miembro(X, [_|Z]):-
+	miembro(X, Z).
